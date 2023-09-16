c82d0e02adde0f828f8e769b3c3b55c40cb4343b
Maksym Kanupa <maksymkanupa@gmail.com>
2023-09-16 11:29:37 +0300

    Implement methods

--- Lab1.SortedList/SortedList.cs
+++ Lab1.SortedList/SortedList.cs
@@ -5,7 +5,7 @@ namespace Lab1.SortedList;
 public class SortedList<T> : ICollection<T> where T : IComparable<T>
 {
     private Node<T>? _head;
-    public int Count { get; }
+    public int Count { get; private set; }
     public bool IsReadOnly => false;
     
     public IEnumerator<T> GetEnumerator()
@@ -25,32 +25,96 @@ public class SortedList<T> : ICollection<T> where T : IComparable<T>
 
     public void Add(T item)
     {
-        throw new NotImplementedException();
+        Count++;
+        var newNode = new Node<T>
+        {
+            Value = item
+        };
+
+        if (_head is null || item.CompareTo(_head.Value) < 0)
+        {
+            newNode.Next = _head;
+            _head = newNode;
+            return;
+        }
+
+        var node = _head;
+        while (node.Next is not null && item.CompareTo(node.Next.Value) > 0)
+        {
+            node = node.Next;
+        }
+
+        newNode.Next = node.Next;
+        node.Next = newNode;
     }
 
-    public T? Find(Predicate<T> predicate)
+    public T? Find(Predicate<T> match)
     {
-        throw new NotImplementedException();
+        foreach (var item in this)
+        {
+            if (match(item))
+            {
+                return item;
+            }
+        }
+
+        return default;
     }
 
     public void Clear()
     {
-        throw new NotImplementedException();
+        _head = null;
+        Count = 0;
     }
 
     public bool Contains(T item)
     {
-        throw new NotImplementedException();
+        foreach (var node in this)
+        {
+            if (node.CompareTo(item) == 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public void CopyTo(T[] array, int arrayIndex)
     {
-        throw new NotImplementedException();
+        var i = arrayIndex;
+        foreach (var item in this)
+        {
+            array[i] = item;
+            i++;
+        }
     }
 
     public bool Remove(T item)
     {
-        throw new NotImplementedException();
+        var node = _head;
+        if (node is null) return false;
+        
+        if (node.Value.CompareTo(item) == 0)
+        {
+            Count--;
+            _head = node.Next;
+            return true;
+        }
+        
+        while (node.Next is not null)
+        {
+            if (node.Next.Value.CompareTo(item) == 0)
+            {
+                Count--;
+                node.Next = node.Next.Next;
+                return true;
+            }
+            
+            node = node.Next;
+        }
+
+        return false;
     }
 }
 
