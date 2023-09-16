9a00ba0b0b045a30df9d2a5540bbb33dece4d354
Maksym Kanupa <maksymkanupa@gmail.com>
2023-09-16 11:24:38 +0300

    Add SortedList class

---
+++ Lab1.SortedList/SortedList.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+namespace Lab1.SortedList;
+
+public class SortedList<T> : ICollection<T> where T : IComparable<T>
+{
+    private Node<T>? _head;
+    
+    public IEnumerator<T> GetEnumerator()
+    {
+        
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T? Find(Predicate<T> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count { get; }
+    public bool IsReadOnly { get; }
+}
+
+public class Node<T>
+{
+    public required T Value { get; set; }
+    public Node<T>? Next { get; set; }
+}
