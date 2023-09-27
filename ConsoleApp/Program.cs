29c2b30c9bbcda05f98d8a9227f4e6cae287431e
Maksym Kanupa <maksymkanupa@gmail.com>
2023-09-27 11:48:10 +0300

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,8 @@ sortedList.OnAdd += (sender, args) => Console.WriteLine("Element added!");
 sortedList.OnRemove += (sender, args) => Console.WriteLine("Element removed!");
 sortedList.OnClear += (sender, args) => Console.WriteLine("List cleared!");
 
+Console.WriteLine(sortedList.Count);
+Console.WriteLine("Add items");
 sortedList.Add(2);
 sortedList.Add(1);
 sortedList.Add(-1);
@@ -13,11 +15,13 @@ sortedList.Add(0);
 sortedList.Add(0);
 sortedList.Add(2);
 sortedList.Add(-9);
+Console.WriteLine(sortedList.Count);
 
 sortedList.Remove(0);
-var nine = sortedList.Find(x => x == -9);
 
-// sortedList.CopyTo(arr, 0);
+var arr = new int[sortedList.Count];
+sortedList.CopyTo(arr, 0);
+Console.WriteLine(string.Join(", ", arr));
 
 Console.WriteLine(sortedList.Count);
 Console.WriteLine(sortedList);
