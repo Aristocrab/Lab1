99023f4c829f3df7c590d05ea89eed78c86ac4b4
Aristocrab <maksymkanupa@gmail.com>
2023-10-04 20:10:41 +0300

    Add tests for all methods

--- Lab1.SortedList.Tests/SortedListTests.cs
+++ Lab1.SortedList.Tests/SortedListTests.cs
@@ -1,5 +1,244 @@
 ﻿namespace Lab1.SortedList.Tests;
+using FluentAssertions;
 
 public class SortedListTests
 {
+    [Fact]
+    public void Add_AddsItemToSortedList()
+    {
+        // Arrange
+        var sortedList = new SortedList<int>();
+
+        // Act
+        sortedList.Add(3);
+        sortedList.Add(1);
+        sortedList.Add(2);
+
+        // Assert
+        sortedList.Should().BeInAscendingOrder();
+        sortedList.Should().HaveCount(3);
+        sortedList.Should().ContainInOrder(1, 2, 3);
+    }
+    
+    [Fact]
+    public void AddNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var sortedList = new SortedList<string>();
+
+        // Act
+        var addAction = () => sortedList.Add(null!);
+
+        // Assert
+        addAction.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Remove_RemovesItemFromSortedList()
+    {
+        // Arrange
+        var sortedList = new SortedList<string>();
+        sortedList.Add("a");
+        sortedList.Add("b");
+        sortedList.Add("c");
+        sortedList.Add("d");
+
+        // Act
+        var result = sortedList.Remove("c");
+
+        // Assert
+        result.Should().BeTrue();
+        sortedList.Should().HaveCount(3);
+        sortedList.Should().ContainInOrder("a", "b", "d");
+    }
+    
+    [Fact]
+    public void Remove_ReturnsFalseIfNoItems()
+    {
+        // Arrange
+        var sortedList = new SortedList<string>();
+
+        // Act
+        var result = sortedList.Remove("d");
+
+        // Assert
+        result.Should().BeFalse();
+        sortedList.Should().HaveCount(0);
+    }
+    
+    [Fact]
+    public void Remove_ReturnsFalseIfItemsIsNotInList()
+    {
+        // Arrange
+        var sortedList = new SortedList<string>();
+        sortedList.Add("a");
+
+        // Act
+        var result = sortedList.Remove("d");
+
+        // Assert
+        result.Should().BeFalse();
+        sortedList.Should().HaveCount(1);
+    }
+    
+    [Fact]
+    public void Remove_ReturnsTrueIfItemIsInHead()
+    {
+        // Arrange
+        var sortedList = new SortedList<string>();
+        sortedList.Add("a");
+
+        // Act
+        var result = sortedList.Remove("a");
+
+        // Assert
+        result.Should().BeTrue();
+        sortedList.Should().HaveCount(0);
+    }
+    
+    [Fact]
+    public void RemoveNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var sortedList = new SortedList<string>();
+        sortedList.Add("a");
+        sortedList.Add("b");
+        sortedList.Add("c");
+
+        // Act
+        var removeAction = () => sortedList.Remove(null!);
+
+        // Assert
+        removeAction.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Find_ReturnsItemMatchingPredicate()
+    {
+        // Arrange
+        var sortedList = new SortedList<int>();
+        sortedList.Add(1);
+        sortedList.Add(2);
+        sortedList.Add(3);
+
+        // Act
+        var result = sortedList.Find(x => x == 2);
+
+        // Assert
+        result.Should().Be(2);
+    }
+    
+    [Fact]
+    public void Find_ReturnsNullIfNoItemMatchesPredicate()
+    {
+        // Arrange
+        var sortedList = new SortedList<string>();
+        sortedList.Add("a");
+        sortedList.Add("b");
+        sortedList.Add("c");
+
+        // Act
+        var result = sortedList.Find(x => x == "e");
+
+        // Assert
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void Clear_RemovesAllItemsFromSortedList()
+    {
+        // Arrange
+        var sortedList = new SortedList<int>();
+        sortedList.Add(1);
+        sortedList.Add(2);
+        sortedList.Add(3);
+
+        // Act
+        sortedList.Clear();
+
+        // Assert
+        sortedList.Should().BeEmpty();
+    }
+    
+    [Fact]
+    public void Contains_ReturnsTrueIfItemExists()
+    {
+        // Arrange
+        var sortedList = new SortedList<string>();
+        sortedList.Add("apple");
+        sortedList.Add("banana");
+        sortedList.Add("cherry");
+
+        // Act
+        var contains = sortedList.Contains("banana");
+        
+        // Assert
+        contains.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Contains_ReturnsFalseIfItemDoesNotExist()
+    {
+        // Arrange
+        var sortedList = new SortedList<int>();
+        sortedList.Add(1);
+        sortedList.Add(3);
+        sortedList.Add(5);
+
+        // Act
+        var contains = sortedList.Contains(2);
+        
+        // Assert
+        contains.Should().BeFalse();
+    }
+
+    [Fact]
+    public void CopyTo_CopiesItemsToDestinationArray()
+    {
+        // Arrange
+        var sortedList = new SortedList<int>();
+        sortedList.Add(1);
+        sortedList.Add(2);
+        sortedList.Add(3);
+        var destinationArray = new int[3];
+
+        // Act
+        sortedList.CopyTo(destinationArray, 0);
+
+        // Assert
+        destinationArray.Should().ContainInOrder(1, 2, 3);
+    }
+    
+    [Fact]
+    public void CopyTo_ThrowsArgumentNullExceptionIfArrayIndexIsNegative()
+    {
+        // Arrange
+        var sortedList = new SortedList<int>();
+        sortedList.Add(1);
+        sortedList.Add(2);
+        sortedList.Add(3);
+        var destinationArray = new int[3];
+
+        // Act
+        var copyAction = () => sortedList.CopyTo(destinationArray, -1);
+        
+        // Assert
+        copyAction.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void CopyTo_ThrowsArgumentExceptionIfDestinationArrayIsTooShort()
+    {
+        // Arrange
+        var sortedList = new SortedList<int>();
+        sortedList.Add(1);
+        sortedList.Add(2);
+        var destinationArray = new int[1];
+
+        // Act
+        var copyAction = () => sortedList.CopyTo(destinationArray, 0);
+        
+        // Assert
+        copyAction.Should().Throw<ArgumentException>();
+    }
 }
